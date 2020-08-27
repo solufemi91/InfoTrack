767201e8e26407d1fb41cabb6e2b0b0a75bbc479
Steve Olufemi <solufemi-olukanni@hotmail.com>
2020-08-27 11:36:41 +0100

    reading date from store

---
+++ src/Components/FormComponent.jsx
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+
+class formComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <form id="myForm">
+                <label htmlFor="SearchPhrase">Search Phrase:</label>
+                <input type="text" id="SearchPhrase" name="SearchPhrase"></input><br></br>
+                <label htmlFor="Url">Url:</label>
+                <input type="text" id="Url" name="Url"></input><br></br>
+                <input onClick={this.props.search} defaultValue="Submit"></input><br></br>
+                <input type="text" id="Result" name="Result" value={this.props.numberPositions} readOnly></input>
+            </form>
+        );
+    }
+
+}
+
+export default formComponent;
--- src/Components/root.jsx
+++ src/Components/root.jsx
@@ -1,26 +1,22 @@
-﻿//import { Reducer } from '../Reducers';
-//import { render } from 'react-dom'
-//import { createStore, compose } from 'redux';
-//import CalendarContainer from '../Containers/CalendarContainer';
-//import { Provider } from 'react-redux';
+﻿import { Reducer } from '../Reducers';
+import { render } from 'react-dom'
+import { createStore, compose } from 'redux';
+import FormComponentContainer from '../Containers/formComponentContainer';
+import { Provider } from 'react-redux';
 
 
-//const enhancers = compose(
-//    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//);
+const enhancers = compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-//const store = createStore(
-//    Reducer, enhancers
-//);
+const store = createStore(
+    Reducer, enhancers
+);
 
-//const domContainer = document.getElementById('calendarContainer');
-//const data = domContainer.getAttribute('data-react-model');
-//const obj = JSON.parse(data);
+const domContainer = document.getElementById('reactContainer');
 
-//store.dispatch({ type: 'INIT', data: obj });
-
-//render(
-//    <Provider store={store}>
-//        <CalendarContainer />,
-//    </Provider>,
-//    domContainer); 
+render(
+    <Provider store={store}>
+        <FormComponentContainer />,
+    </Provider>,
+    domContainer); 
